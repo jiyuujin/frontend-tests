89f1c57c292ee77c4eb42a1c1a33880260b7f43c
jiyuujin <jiyuujinunite@gmail.com>
2021-07-17 08:13:24 +0900

    feat: payment

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { findVowels } from './services/vowelService'
+import { getPaymentTotal } from './services/paymentService'
 
 const vowelCount = findVowels('HELLO')
 console.log(vowelCount)
+
+const priceA = getPaymentTotal(['210', '430', '760'], true)
+const priceB = getPaymentTotal(['800', '250'], false)
+const priceC = getPaymentTotal(['abc'], true)
+const priceD = getPaymentTotal(['abc'], false)
+console.log(priceA)
+console.log(priceB)
+console.log(priceC)
+console.log(priceD)
